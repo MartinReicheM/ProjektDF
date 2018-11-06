13957b17cfa27f10c7e4404f7d87431b7710e55c
Martin Reiche <reichex26@gmail.com>
2018-11-06 17:01:34 +0100

    Initial commit

---
+++ DF.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+
+entity DF is 
+port(
+			reset		: in std_logic;
+			mode		: in std_logic;
+			increase : in std_logic;
+			decrease : in std_logic;
+			clock_400: in std_logic
+
+
+);
+end entity DF;
+
+architecture rtl of DF is 
+begin
+
+end architecture rtl;
