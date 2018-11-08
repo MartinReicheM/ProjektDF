7bb5c6e3d7b7db13acd2ab026fc495c106e0fade
Martin Reiche <reichex26@gmail.com>
2018-11-08 15:11:22 +0100

    Restart step 1

---
+++ DigitalFilter.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+
+entity DigitalFilter is 
+port(
+		input : in std_logic
+);
+end entity;
+
+architecture rtl of DigitalFilter is 
+
+begin 
+
+end architecture rtl;
